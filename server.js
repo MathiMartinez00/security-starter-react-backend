d8fd81259c67f05296df6e2f6d1d46aed6815d2c
MathiMartinez00 <mathias.martinez018@gmail.com>
2023-06-10 16:25:33 -0400

    Eliminar restriccion en modo produccion

--- server.js
+++ server.js
@@ -1,32 +1,18 @@
 const jsonServer = require('json-server')
 const auth = require('json-server-auth')
-const clone = require('clone')
-const data = require('./db.json')
 
-const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create()
-
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
-    _isFake: isProductionEnv
-})
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.db = router.db
 
 server.use(middlewares)
-
-server.use((req, res, next) => {
-    if (req.path !== '/')
-        router.db.setState(clone(data))
-    next()
-})
-
 server.use(auth)
 server.use(router)
+
 server.listen(process.env.PORT || 8000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
